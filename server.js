8c98006778d198c76a4c439f61ac91d245ccda07
Manu3 <manu.kapoor91@gmail.com>
2017-12-07 19:59:29 +0530

    anu commited

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 9090;
 var app = express();
 
 //to render the page -- help.html
@@ -59,6 +60,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(9090, () => {
-  console.log('server is up');
+app.listen(port, () => {
+  console.log(`server is up at port: ${port}`);
 });
